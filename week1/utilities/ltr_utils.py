8e29069b090e2dec0a33f875a289e4f7f513cc73
Jean Silva <jeancsil@gmail.com>
2022-10-30 19:30:16 +0100

    Last test with score mode

--- week1/utilities/ltr_utils.py
+++ week1/utilities/ltr_utils.py
@@ -21,11 +21,11 @@ def create_rescore_ltr_query(user_query: str, query_obj, click_prior_query: str,
                     },
                     "model": ltr_model_name,
                     # Since we are using a named store, as opposed to simply '_ltr', we need to pass it in
-
                     "store": ltr_store_name,
                     "active_features": []
                 }
             },
+            "score_mode": "total",
             "rescore_query_weight": rescore_query_weight,
             "query_weight": main_query_weight
         }
